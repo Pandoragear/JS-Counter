5e7c65b191b79501ee03eacd6efcaa1d37b6a9ed
pandoragear <andy.dang.it@gmail.com>
2021-02-06 22:31:44 -0600

    Add if else set num + / - / 0

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if(styles.contains('decrease' )){
-        count--;
-        value.textContent = count;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
     }
+    value.textContent = count;
   });
 });
