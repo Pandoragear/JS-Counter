e536ec30c5cc90fb6103cdfe29aea29ca99b92ca
pandoragear <andy.dang.it@gmail.com>
2021-02-06 22:26:44 -0600

    Add decrease value in btn Function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// set inital count 
-let count = 0; 
+// set inital count
+let count = 0;
 
-// select value and buttons 
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (btn){
-    btn.addEventListener('click', function(e){
-console.log(e.currentTarget.classList);
-    })
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease' )){
+        count--;
+        value.textContent = count;
+    }
+  });
 });
-
