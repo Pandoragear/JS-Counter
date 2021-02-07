d9cea4b3bec68b6eb76a47b37aebc376316fb123
pandoragear <andy.dang.it@gmail.com>
2021-02-06 22:24:11 -0600

    Add btns function

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
-console.log(btns);
+btns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+console.log(e.currentTarget.classList);
+    })
+});
 
